54fef8d07c4e79381c0aaeffb431d07efabad531
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-02 15:43:42 +0800

    feat: Add YouTube video processing and vector store creation
      - Added create_vector_db_from_youtube_url function to process YouTube videos and create FAISS vector stores from their transcripts.
      - Tested the function with a sample YouTube URL.

--- langchain_helper.py
+++ langchain_helper.py
@@ -12,10 +12,36 @@ from langchain.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 # Module to create and manage vector stores using FAISS.
 from langchain.vectorstores import FAISS
-
+# Module to generating text embeddings using OpenAI's models
+from langchain_community.embeddings import OpenAIEmbeddings
 
 # Loading environment variables from a .env file.
 load_dotenv()
 
 # Creating an instance of the OpenAIEmbeddings class to generate embeddings for text data.
 embeddings = OpenAIEmbeddings()
+
+
+def create_vector_db_from_youtube_url(video_url: str) -> FAISS:
+    # Create a loader object to fetch the YouTube video transcript.
+    loader = YoutubeLoader.from_youtube_url(video_url)
+
+    # Load the transcript of the YouTube video.
+    transcript = loader.load()
+
+    # Create a text splitter to divide the transcript into smaller chunks.
+    # chunk_size is the maximum size of each chunk, and chunk_overlap is the overlap between chunks.
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 100)
+
+    # Split the transcript into smaller, manageable chunks.
+    docs = text_splitter.split_documents(transcript)
+
+    # Create a FAISS vector store from the document chunks, using the specified embeddings.
+    db = FAISS.from_documents(docs, embeddings)
+
+    # Return the FAISS vector store containing the embedded document chunks.
+    return db
+
+
+video_url = "https://youtu.be/A9W6FAQPVuA?si=qshtmH3E_ah9QvbT"
+print(create_vector_db_from_youtube_url(video_url))
